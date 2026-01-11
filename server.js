d8776accbed55a2083dc46bedf4c62c06a6c429c
prasanna-ofcc <prasannaa.ofcc@gmail.com>
2026-01-11 13:03:19 +0530

    change

--- server.js
+++ server.js
@@ -2,10 +2,23 @@
 const express = require("express");
 // create an instance of express
 const app = express();
-//define the route
-app.get('/',(req, res)=>{
-    res.send("Hello Prasanna, welcome Aboard");
-})
+//sample in memory todo list
+let todos=[];
+
+//create a todo item
+app.post("todos", (req, res)=>{
+    const {body, description}=req.body;
+    const newtodo={
+        id:todos.length + 1,
+        title,
+        description
+    };
+    todos.push(newtodo);
+    console.log(todos);
+    res.status(201).json(newtodo);
+
+});
+
 //start the server
 const port = 3000;
 app.listen(port, ()=>{
