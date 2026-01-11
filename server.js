cf4b2594d21a63337b68e8324a3ca2e4d31a54e3
prasanna-ofcc <prasannaa.ofcc@gmail.com>
2026-01-11 12:51:30 +0530

    The server.js changes

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 //express
+const express = require("express");
+// create an instance of express
+const app = express();
+//define the route
+app.get('/',(req, res)=>{
+    res.send("Hello Prasanna, welcome Aboard");
+})
+//start the server
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Server is running on http://localhost:${port}`);
+});
